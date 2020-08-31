98a67d4a9ee943ab617db95e77b56ad1fd8e293d
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2020-08-30 22:42:52 -0500

    footer

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const postcssNesting = require('postcss-nesting');
+
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssImport = require('postcss-import')({
     plugins:[
@@ -9,18 +11,25 @@ const fontMagician = require('postcss-font-magician');
 const assets  = require('postcss-assets');
 const stylelint = require("stylelint");
 const mqpacker = require("css-mqpacker");
+const rucksack = require('rucksack-css');
+const postcssExtendRule = require('postcss-extend-rule');
+const calc = require("postcss-calc")
+
 
 
 module.exports = {
   plugins: [
+
     postcssImport,
+    require('postcss-nested'),
+    calc,
     assets({
         loadPaths: ['images/']
     }),
     postcssPresetEnv({
         stage:3,
-        autoprefixer: { grid: true },
-           browsers: 'last 2 versions',
+        autoprefixer: { grid: true , flex:false},
+           browsers: ['last 2 versions', '> 5%', 'ie 8'],
            preserve: false,
     }),
     fontMagician({
@@ -33,9 +42,13 @@ module.exports = {
         },
         foundries: ['google']
    }),
+
+   postcssNesting,
+   postcssExtendRule,
+   rucksack,
    mqpacker,
-   require('cssnano')({
-    preset: 'default',
-}),
+//    require('cssnano')({
+//     preset: 'default',
+// }),
   ]
 }
